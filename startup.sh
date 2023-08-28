4a31d6d2fa594107c4f2397b84fc68f26efb96d8
EC2 Default User <ec2-user@ip-172-31-4-122.us-east-2.compute.internal>
2023-08-28 17:21:34 +0000

    update port to 8080

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /home/ec2-user/gangonem_revamp
-export SERVER_PORT=80
+export SERVER_PORT=8080
 #nohup java -jar target/GANGONEM-0.0.1-SNAPSHOT.jar &
 java -jar target/GANGONEM-0.0.1-SNAPSHOT.jar
